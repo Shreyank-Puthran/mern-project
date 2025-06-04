1a780edb3a608c74321f616ea2d8cb39364d222e
Shreyank Puthran <shreyankputhran@gmail.com>
2025-06-04 20:05:04 +0530

    Fix slider text

--- soccer-frontend/src/Main/components/imageSlider.jsx
+++ soccer-frontend/src/Main/components/imageSlider.jsx
@@ -46,7 +46,7 @@ const ImageSlider = () => {
     flex flex-col items-center sm:items-end justify-center 
     text-white font-bold
     transition-opacity duration-1000 text-3xl
-    px-10
+    px-10 md:px-20
     ${
       loaded && index === current
         ? "opacity-100 scale-100"
